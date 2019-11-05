f1bd40a325336cc660b3c94b9f243a3466ec3b3c
Jon Bent <jon.bent@me.com>
2019-11-04 20:26:46 -0800

    Fix Auth0 secret errors

--- config/application.rb
+++ config/application.rb
@@ -5,18 +5,16 @@ require 'rails/all'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-
-if ['development', 'test'].include? ENV['RAILS_ENV']
+if ['development', 'test'].include? Rails.env
   Dotenv::Railtie.load
 end
-
 HOSTNAME = ENV['HOSTNAME']
 
 module DelivChallenge
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
-
+    
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
